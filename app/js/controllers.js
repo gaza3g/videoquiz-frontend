2c70869fda6f6da415cbde5151e93744b493bf20
Ghazaly <gazaly@gmail.com>
2015-04-06 14:05:26 +0800

    Managed to figure out promises.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,26 +15,63 @@ videoQuizControllers.controller('QuizCtrl', ['$scope', '$sce', '$routeParams','$
 
 	    $scope.quizid = $routeParams.quizid; 
 	    $scope.puid = $routeParams.puid;
+	    $scope.videoUrl = '';
 
 	    $scope.headingTitle = 'Our Story in 1 Minute';
 
 	    $scope.webServiceUrl = "http://uat.asknlearn.com/api/quiz/";
 	    $scope.pollServerUrl = "http://uat.asknlearn.com/dev/Webservice/Quiz/QZN_QuizWebService.asmx/QZN_SaveUserResponse";
 
-	    $http.get($scope.webServiceUrl  + $scope.quizid + '/video').success(function(data) {
-	    	alert(data);
-		    // $scope.users = data;
-		});
+	 //    $http.get($scope.webServiceUrl  + $scope.quizid + '/video').success(function(data) {
 
-	    console.log("PUID:" + $scope.puid);
-	    console.log("QuizID:" + $scope.quizid);
+	 //    	$scope.videoUrl = data;
 
+
+		// 	$scope.config = {
+		// 		autoHide: false,
+		// 		autoHideTime: 3000,
+		// 		sources: [
+		// 			{src: $sce.trustAsResourceUrl("http://uat.asknlearn.com/EdulearnNETUpload/dev/learningobject/File/0f41ece8-c444-6169-570d-0f4044dc1c14/Google_Goggles.mp4"), type: "video/mp4"},
+		// 		],
+		// 		theme: {
+		// 			url: "videogular.css"
+		// 		},
+		// 		plugins: {
+		// 			questions: {
+		// 				theme: {
+		// 					url: "css/quiz.css"
+		// 				},
+		// 				data:{
+		// 					url: "bower_components/video-quiz/get_questions.js?" + $scope.quizid,
+		// 				},
+		// 				webService:{
+		// 					url: $scope.webServiceUrl,
+		// 				},
+		// 				pollServer:{
+		// 					url: $scope.pollServerUrl,
+		// 				}
+		// 			},
+		// 			cuepoints: {
+		// 				theme: {
+		// 					url: "bower_components/videogular-cuepoints/cuepoints.css",
+		// 				},
+		// 			},
+		// 			analytics: {
+		// 				servers: [
+		// 					"http://localhost:5001/"
+		// 				]
+		// 			}
+		// 		}
+		// 	};
+
+		// });
+// http://uat.asknlearn.com/EdulearnNETUpload/dev/learningobject/File/0f41ece8-c444-6169-570d-0f4044dc1c14/Google_Goggles.mp4
 
 		$scope.config = {
 			autoHide: false,
 			autoHideTime: 3000,
 			sources: [
-				{src: $sce.trustAsResourceUrl("https://videoquizstorage.blob.core.windows.net/asset-9e59093c-8fb3-4f17-b798-570d5998514c/video.mp4?sv=2012-02-12&sr=c&si=2fb819f8-081b-487b-970d-c3d24c91b61b&sig=QMjiVj9H3MlyUACk0qi2HZuWUfiI4ADp7z5rXeQZ2Vw%3D&st=2015-02-11T03%3A39%3A44Z&se=2017-02-10T03%3A39%3A44Z"), type: "video/mp4"},
+				// {src: $sce.trustAsResourceUrl("http://uat.asknlearn.com/EdulearnNETUpload/dev/learningobject/File/0f41ece8-c444-6169-570d-0f4044dc1c14/Google_Goggles.mp4"), type: "video/mp4"},
 			],
 			theme: {
 				url: "videogular.css"
@@ -66,5 +103,22 @@ videoQuizControllers.controller('QuizCtrl', ['$scope', '$sce', '$routeParams','$
 				}
 			}
 		};
+	    // console.log("PUID:" + $scope.puid);
+	    // console.log("QuizID:" + $scope.quizid);
+	    // if( $scope.config.sources.length > 0)
+		   //  console.log($scope.config.sources[0].src.toString());
+
+	    $http.get($scope.webServiceUrl  + $scope.quizid + '/video').success(function(data) {
+	    	$scope.config.sources = [{src: $sce.trustAsResourceUrl(data), type: "video/mp4"}];
+	    	console.log("cool")
+	    	if( $scope.config.sources.length > 0)
+	    	{
+			    console.log("lol " + $scope.config.sources[0].src.toString());
+	    	}
+	    });
+
+
+
+
 	}
 ]);
